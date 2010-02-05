cbefa7b6b9f40cdd0a91ad80f88e3ee1017ad5b2
System Administrator <root@testing.huronhs.com>
2010-02-05 09:29:05 -0500

    Added file mjhsmultimedia

---
+++ manifests/classes/mjhsmultimedia.pp
@@ -0,0 +1,20 @@
+#/etc/puppet/manifests/classes/mjhsmultimedia.pp
+
+class mjhsmultimedia {
+
+	#  Includes
+	include studentdata
+	include general_image
+
+	# Package Names
+	$finalcut = "FinalCut.dmg"
+	$googleearth = "GoogleEarth.dmg"
+
+	# Set Package resource defaults for OS X clients
+	Package{ensure => installed,provider => pkgdmg}	
+
+	# Install Specialized Packages
+	package{"$finalcut": source => "$pkg_base/$finalcut",}
+	package{"$googleearth": source => "$pkg_base/$googleearth",}
+
+} # End of Class
