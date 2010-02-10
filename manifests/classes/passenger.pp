cea5ca655d0c69e53797f9df7c09780c24862401
System Administrator <root@testing.huronhs.com>
2010-02-10 14:39:15 -0500

    Attached Class to passenger.pp file

--- manifests/classes/passenger.pp
+++ manifests/classes/passenger.pp
@@ -1,3 +1,4 @@
+class passenger {
 
 file { ["/etc/puppet/rack", "/etc/puppet/rack/public"]:
   ensure => directory,
@@ -5,6 +6,7 @@ file { ["/etc/puppet/rack", "/etc/puppet/rack/public"]:
   owner => puppet,
   group => puppet,
 }
+
 file { "/etc/puppet/rack/config.ru":
   ensure => present,
   source => "puppet:///files/config.ru",
@@ -45,11 +47,7 @@ exec { "/usr/bin/passenger-install-apache2-module --auto":
       subscribe => Package["passenger"],
       before => Service["apache2"],
       require => Package["passenger"],
+      require => File["/etc/apache2/httpd.conf"],
 }
 
-notice("You need to manually enable mod_passenger.so for Apache.")
-notice("Usually, you put these config stanzas into httpd.conf:")
-notice("   LoadModule passenger_module /var/lib/gems/1.8/gems/passenger-2.2.2/ext/apache2/mod_passenger.so")
-notice("   PassengerRoot /var/lib/gems/1.8/gems/passenger-2.2.2")
-notice("   PassengerRuby /usr/bin/ruby1.8")
-notice("--------------------------------------------------------")
+} #End of Class
