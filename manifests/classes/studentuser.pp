b71efe301d2b1c378b18a46b5fe11430e3a65512
System Administrator <root@testing.huronhs.com>
2010-02-11 18:26:10 -0500

    Added entries to studentsaccount.plist

--- manifests/classes/studentuser.pp
+++ manifests/classes/studentuser.pp
@@ -1,12 +1,15 @@
 class studentuser {
 
-User { provider => "directoryservice" }
+# User { provider => "directoryservice" }
 
-user { "Students":
-	ensure => present,
-	uid => 502,
-	password => "tigers",
-	shell => "/bin/bash";
+user { 'students':
+	comment => 'Student User',
+	home => '/Users/students',
+	gid => '20',
+	ensure => 'present',
+	uid => '502',
+	password => '0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000D34561F882B4BF7F93B1BE6DD1A7458A40C4FD0BF9057FC0000000000000000000000',
+	shell => '/bin/bash';
 	}
 
 } # End of Class
