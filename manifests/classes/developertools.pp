bc2524c165de80a990d6dc4286aaeee8a6aa7fdb
Gary Larizza <gary@huronhs.com>
2010-03-16 13:44:13 -0400

    Fixed Developer Tools class - removed period from variable

--- manifests/classes/developertools.pp
+++ manifests/classes/developertools.pp
@@ -3,18 +3,18 @@ class developertools {
 	Package{ensure => installed,provider => pkgdmg}
 
 	# Package Names
-	$devtools10.6 = "devtools313.dmg"
-	$devtools10.5 = "developertools10.5.dmg"
+	$devtools106 = "devtools313.dmg"
+	$devtools105 = "developertools10.5.dmg"
 	# Package Calls
 
 	case $macosx_productversion_major {
 		10.5: { 
-			package{"$devtools10.5":
+			package{"$devtools105":
 				source => "$pkg_base/$devtools10.5",
 				}
 		       }			
 		10.6: { 
-			package{"$devtools10.6":
+			package{"$devtools106":
 				source => "$pkg_base/$devtools10.6",
 	               	}
 	}
