58f18eadb0847357fd226c401d3242b48b39e215
Gary Larizza <gary@huronhs.com>
2010-03-16 13:33:43 -0400

    Modified nodes.pp to add wesdocs server

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -6,7 +6,9 @@ node 'boe.huronhs.com' inherits basenode {
 node "demomini.huronhs.com" {
 	include nrpe
 }
-
+node "wesdocs.huronhs.com" {
+	include nrpe
+}
 node default {
 	include puppet_LaunchDaemon
 }
