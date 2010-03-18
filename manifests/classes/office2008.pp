f40b82472bd092d5f8b3a96d200038debb9f146d
Gary Larizza <gary@huronhs.com>
2010-03-18 14:59:38 -0400

    Final office2008 class revisions - added config packages and dependencies into the mix.

--- manifests/classes/office2008.pp
+++ manifests/classes/office2008.pp
@@ -4,20 +4,39 @@
 #
 
 class office2008 {
+
 	# Package Names
 	$office = "Office2008.dmg"
+	$update1210 = "Office2008-1210UpdateEN.dmg"
 	$update1220 = "Office2008-1220UpdateEN.dmg"
 	$update1223 = "Office2008-1223UpdateEN.dmg"
 	$update1224 = "Office2008-1224UpdateEN.dmg"
-
+	$officeconfig = "Office2008Config.dmg"
+	$officestudent = "Office2008Studentconfig.dmg"
+	$officesave = "Office2008Savesettings.dmg"
+	
 	# Set Package resource defaults for OS X clients
 	#Package{ensure => installed,provider => pkgdmg}
 
 	# Package Calls
 	package{"$office": 
 		source => "$pkg_base/$office",
-		before => Package[$update1220],
 		}
+        package{"$officeconfig":
+                source => "$pkg_base/$officeconfig",
+                before => Package[$officestudent],
+                require => Package[$office],
+                } 	
+	package{"$officestudent":
+		source => "$pkg_base/$officestudent",
+		before => Package[$officesave],
+		require => Package[$officeconfig],
+		}
+	package{"$officesave":
+		source => "$pkg_base/$officesave",
+		before => Package[$update1220],
+		require => Package[$officestudent],
+	}
 	package{"$update1220":
                 source => "$pkg_base/$update1220",
                 require => Package[$office],
@@ -33,4 +52,5 @@ class office2008 {
                 require => Package[$update1223],
                 }
 
-} #End of Class
+}
+
