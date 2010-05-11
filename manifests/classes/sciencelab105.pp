8ec78c9e2562471e6596ba4072e9f07beeb6e79f
Gary Larizza <gary@huronhs.com>
2010-05-11 12:43:03 -0400

    Added Alice to Science Lab 105

--- manifests/classes/sciencelab105.pp
+++ manifests/classes/sciencelab105.pp
@@ -5,4 +5,13 @@ class sciencelab105 {
 	#  Include the studentdata class which creates our Student Data Folder
 	include sciencelab101
 
+	#Package Names
+	$alice = "Alice.dmg"
+
+	# Set Package Resource
+	Package{ensure => installed,provider => pkgdmg}
+
+	# Package Calls
+	package{"$alice": source => "$pkg_base/$alice",}
+
 } # End of Class
