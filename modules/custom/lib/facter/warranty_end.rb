ca04b8e5ba1a53556e78f66000acc782e82cdc37
Gary Larizza <gary@huronhs.com>
2010-06-07 13:05:50 -0400

    Added purchase_date into the warranty facter fact.

--- modules/custom/lib/facter/warranty_end.rb
+++ modules/custom/lib/facter/warranty_end.rb
@@ -16,17 +16,29 @@ item = %x{curl -k -s "https://selfsolve.apple.com/Warranty.do?serialNumber=#{sn}
 #  and enter the remaining information into an array.
 warranty_array = item.strip.split('"')
 
+#  Set the Purchase Date by searching through the Array
+purchase_date = warranty_array.index('PURCHASE_DATE')
+
+Facter.add("purchase_date") do
+  setcode do
+    $purchase_date = warranty_array[purchase_date+2]
+  end
+end
+
 #  First we need to check and see if the machine is out of warranty.  If it is,
 #  return the purchase date.  If it isn't out of warranty, return the Coverage Date
 coverage_check = warranty_array.index('COVERAGE_DESC')
+
 if warranty_array[coverage_check+2].chomp == "Out of Warranty"
-  get_purchase_date = warranty_array.index('PURCHASE_DATE')
+  
   Facter.add("warranty_end") do
     setcode do
-      $warranty_end = warranty_array[get_purchase_date+2]
+      $warranty_end = warranty_array[purchase_date+2]
     end
   end
+  
 else
+  
   position = warranty_array.index('COVERAGE_DATE')
   Facter.add("warranty_end") do
     setcode do
