9f78c2ed3aa250ada06e33fccdcfbecc40da67b1
Gary Larizza <gary@huronhs.com>
2010-06-17 19:28:30 -0400

    Repaired Owner-Group calls and added comma at the end of a password.

--- manifests/classes/hcspuppetmasters.pp
+++ manifests/classes/hcspuppetmasters.pp
@@ -6,15 +6,15 @@ class hcspuppetmasters {
 	file {"/usr/bin/rubyexternalnode.pp":
 		source => "puppet:///files/rubyexternalnode.rb",
 		mode => 0755,
-		owner => root,
-		group => wheel,
+		owner => "root",
+		group => "wheel",
 	}
 	
 	file {"/Library/WebServer/CGI-Executables/pclean.rb":
 	source => "puppet:///files/pclean.rb",
 	mode => 0755,
-	owner => root,
-	group => staff,
+	owner => "root",
+	group => "staff",
 	}
 
 } # End of Class
