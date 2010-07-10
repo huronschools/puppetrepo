7f3680512bf157e783f6c8c65d8d0cc4ba8f23bd
Gary Larizza <gary@huronhs.com>
2010-07-09 20:33:40 -0400

    Disable service call until I figure out how to handle it...

--- manifests/classes/puppetlaunchdaemon.pp
+++ manifests/classes/puppetlaunchdaemon.pp
@@ -2,56 +2,19 @@
 
 class puppet_LaunchDaemon {
 
-	case $macosx_productversion_major {
-		10.4: { 
-			file { "/Library/LaunchDaemons/com.huronhs.puppetconfig.plist":
-				owner => root,
-				group => wheel,
-				mode => 644,
-				source => "puppet:///files/com.huronhs.puppetconfig.plist",
-				ensure => present,
-			} # End of File
+file { "/Library/LaunchDaemons/com.huronhs.puppetconfig.plist":
+	owner => root,
+	group => wheel,
+	mode => 644,
+	source => "puppet:///files/com.huronhs.puppetconfig.plist",
+	ensure => present,
+} # End of File
 
-			service { "com.huronhs.puppetconfig":
-				enable => true,
-				ensure => running,
-				subscribe => File["/Library/LaunchDaemons/com.huronhs.puppetconfig.plist"],
-				require => File["/Library/LaunchDaemons/com.huronhs.puppetconfig.plist"],
-			}# End of Service
-		       }
-		10.5: { 
-			file { "/Library/LaunchDaemons/com.huronhs.puppetconfig.plist":
-				owner => root,
-				group => wheel,
-				mode => 644,
-				source => "puppet:///files/com.huronhs.puppetconfig.plist",
-				ensure => present,
-			} # End of File
-
-			service { "com.huronhs.puppetconfig":
-				enable => true,
-				ensure => running,
-				subscribe => File["/Library/LaunchDaemons/com.huronhs.puppetconfig.plist"],
-				require => File["/Library/LaunchDaemons/com.huronhs.puppetconfig.plist"],
-			}# End of Service
-		       }			
-		10.6: { 
-			file { "/Library/LaunchDaemons/com.huronhs.puppetconfig.plist":
-				owner => root,
-				group => wheel,
-				mode => 644,
-				source => "puppet:///files/com.huronhs.puppetconfig.plist",
-				ensure => present,
-			} # End of File
-
-			service { "com.huronhs.puppetconfig":
-				enable => true,
-				ensure => running,
-				subscribe => File["/Library/LaunchDaemons/com.huronhs.puppetconfig.plist"],
-				require => File["/Library/LaunchDaemons/com.huronhs.puppetconfig.plist"],
-			}# End of Service
-	           }
-	}
-	
+# service { "com.huronhs.puppetconfig":
+# 	enable => true,
+# 	ensure => running,
+# 	subscribe => File["/Library/LaunchDaemons/com.huronhs.puppetconfig.plist"],
+# 	require => File["/Library/LaunchDaemons/com.huronhs.puppetconfig.plist"],
+# }# End of Service
 
 } #End of Class
