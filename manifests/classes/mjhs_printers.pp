474198f806679ea13e17703bf126636af8bef8ae
Gary Larizza <gary@huronhs.com>
2010-07-21 16:45:45 -0400

    Refined mjhs_printers class to add unless and require statements.

--- manifests/classes/mjhs_printers.pp
+++ manifests/classes/mjhs_printers.pp
@@ -5,7 +5,7 @@ class mjhs_printers {
 	exec { "mjhs_office_9050":
 		command => "/usr/sbin/lpadmin -p psm_MJHS_Office_9050 -L MJHS\ Office -D MJHS\ Main\ Office\ 9050\ Copier -v lpd://10.13.2.7/Main_Office_9050_Copier -P /Library/Printers/PPDs/Contents/Resources/HP\ LaserJet\ 9050.gz -E -o printer-is-shared=false",
 		before => File["/etc/cups/ppd/psm_MJHS_Office_9050.ppd"],
-		refreshonly => true,
+		unless => "/usr/bin/lpstat -a psm_MJHS_Office_9050",
 	}
 	
 	file { "/etc/cups/ppd/psm_MJHS_Office_9050.ppd":
@@ -14,6 +14,7 @@ class mjhs_printers {
 		mode => 644,
 		source => "puppet:///files/psm_MJHS_Office_9050.ppd",
 		ensure => present,
+		require => Exec["mjhs_office_9050"],
 	}
 
 } # End of Class
