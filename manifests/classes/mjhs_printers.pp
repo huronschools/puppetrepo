cf4e5e6e250c5a02476286ed89d5d3ade4cd85de
Gary Larizza <gary@huronhs.com>
2010-07-21 10:50:42 -0400

    Fixed a Comma`

--- manifests/classes/mjhs_printers.pp
+++ manifests/classes/mjhs_printers.pp
@@ -4,7 +4,7 @@ class mjhs_printers {
 
 	exec { "mjhs_office_9050":
 		command => "/usr/sbin/lpadmin -p psm_MJHS_Office_9050 -L MJHS\ Office -D MJHS\ Main\ Office\ 9050\ Copier -v lpd://10.13.2.7/Main_Office_9050_Copier -P /Library/Printers/PPDs/Contents/Resources/HP\ LaserJet\ 9050.gz -E -o printer-is-shared=false",
-		before => File["/etc/cups/ppd/psm_MJHS_Office_9050.ppd"]
+		before => File["/etc/cups/ppd/psm_MJHS_Office_9050.ppd"],
 		refreshonly => true,
 	}
 	
