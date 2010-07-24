5d56b34dfd71c5f75b3abd14c3b7fd38a5d72148
Gary Larizza <gary@huronhs.com>
2010-07-24 10:52:09 -0400

    Fixed typo in printers_hhs_g5labbw.

--- manifests/classes/printers/printers_hhs_g5labbw.pp
+++ manifests/classes/printers/printers_hhs_g5labbw.pp
@@ -1,6 +1,6 @@
 #/etc/puppet/manifests/classes/printers/printers_hhs_g5labbw.pp
 
-class printers_mjhs_imaclabbw {
+class printers_hhs_g5labbw {
 
 	exec { "hhs_g5lab_bw":
 		command => "/usr/sbin/lpadmin -p psm_HHS_G5_BW -L HHS\\ Room\\ 099 -D HHS\\ G5\\ Lab\\ Black\\ and\\ White\\ Printer -v lpd://10.13.1.8/HHS_G5_Lab_BW_Printer -P /Library/Printers/PPDs/Contents/Resources/HP\\ LaserJet\\ 4200\\ Series.gz -E -o printer-is-shared=false",
