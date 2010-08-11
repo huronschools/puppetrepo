0142c3af80cef88095295559f5c7824d6041d949
Gary Larizza <gary@huronhs.com>
2010-08-11 07:49:47 -0400

    Added wisimaclab class

---
+++ manifests/classes/wisimaclab.pp
@@ -0,0 +1,16 @@
+#/etc/puppet/manifests/classes/wisimaclab.pp
+
+class wisimaclab {
+
+	#  Includes
+	include studentdata
+	include hslabs
+	include general_image
+	include desktopbackground
+#	include studentuser
+
+	# Package Names
+
+	# Install Required Packages First
+
+} # End of Class
