42355dc55aaa921b1478fe6bcc453a8c3e27fb0b
Gary Larizza <gary@huronhs.com>
2010-09-02 13:10:06 -0400

    Added wes class

--- manifests/classes/wis.pp
+++ manifests/classes/wis.pp
@@ -10,3 +10,14 @@ class wis {
 	include printers_wis_computerlab
 
 } # End of Class
+
+class wes {
+
+	include wes_conf
+	include printers_wis_office9050
+	include printers_wis_officeworkroom
+	include printers_wis_wing2
+	include printers_wis_wing1
+	include printers_wis_computerlab
+
+} # End of Class
