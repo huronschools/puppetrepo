ba8b36b796341a93b7a66b3a0f936b65c6d74586
Gary Larizza <gary@huronhs.com>
2010-09-14 15:59:27 -0400

    Added Hooks file statements

--- manifests/classes/loginhooks.pp
+++ manifests/classes/loginhooks.pp
@@ -3,8 +3,39 @@
 class loginhooks {
 
 	$hooks = "Enable_Hooks-20100914.dmg"
-	Package{ensure => installed,provider => pkgdmg}
-	package{"$hooks": source => "$pkg_base/$hooks",}
+	
+	package{"$hooks": 
+		source => "$pkg_base/$hooks",
+		before => File["/etc/hooks/LICachesRedirect.hook"],
+		ensure => installed,
+		provider => pkgdmg,
+		}
+	file { "/etc/hooks/LICachesRedirect.hook":
+		ensure => file,
+		require => Package["$hooks"],
+		owner => "root",
+		group => "admin",
+		mode => 755,
+		source => "puppet:///files/Loginhooks/LICachesRedirect.hook",
+	}
+	file { "/etc/hooks/LILogger.hook":
+		ensure => file,
+		require => Package["$hooks"],
+		owner => "root",
+		group => "admin",
+		mode => 755,
+		source => "puppet:///files/Loginhooks/LILogger.hook",
+	}
+	file { "/etc/hooks/LOLogger.hook":
+		ensure => file,
+		require => Package["$hooks"],
+		owner => "root",
+		group => "admin",
+		mode => 755,
+		source => "puppet:///files/Loginhooks/LOLogger.hook",
+	}
+	
+	
 
 } # End of Class
 
