af9c4b05b783ccda83a9a370600bf6f445e1b747
Gary Larizza <gary@huronhs.com>
2010-11-22 13:20:24 -0500

    Revised pkg_base.rb to add a location fact. Awesome.

--- modules/custom/lib/facter/pkg_base.rb
+++ modules/custom/lib/facter/pkg_base.rb
@@ -7,26 +7,38 @@ Facter.add("pkg_base") do
 	if $ip == "0"
 		setcode do
 			$pkg_base = "http://testing.huronhs.com/pkgs/general_image"
+			$location = "SHEL"
 		end
 	elsif $ip == "1"
 		setcode do
 			$pkg_base = "http://helpdesk.huronhs.com/pkgs"
+			$location = "HHS"
 		end
 	elsif $ip == "5"
 		setcode do
 			$pkg_base = "http://helpdesk.huronhs.com/pkgs"
+			$location = "HHS"
 		end
 	elsif $ip == "2"
 		setcode do
 			$pkg_base = "http://mspuppet.huronhs.com/pkgs"
+			$location = "MJHS"
 		end
 	elsif $ip == "3"
 		setcode do
 			$pkg_base = "http://wesreplica.huronhs.com/pkgs"
+			$location = "WIS"
 		end
 	else
 		setcode do
 			$pkg_base = "http://testing.huronhs.com/pkgs/general_image"
+			$location = "UNKNOWN"
 		end
+  end
 end
+
+Facter.add("location") do
+  setcode do
+    $location
+  end
 end
