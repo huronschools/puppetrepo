d0e5bea3ad86ded9f81761dee6b31a0dc3072cf8
Gary Larizza <gary@huronhs.com>
2011-02-08 15:39:08 -0500

    Updated Warranty fact to reflect Apple's URL changes

--- modules/custom/lib/facter/warranty.rb
+++ modules/custom/lib/facter/warranty.rb
@@ -1,30 +1,42 @@
 require 'facter'
+require 'open-uri'
 
-if Facter.value('kernel') == 'Darwin'
-  #  First grab the serial number. For some reason I can't get it from Facter
-  sn  = %x{ioreg  -l | grep IOPlatformSerialNumber | sed -e s/\\"//g}.chomp.split{"="}[-1].chomp
-  url = "https://selfsolve.apple.com/Warranty.do?serialNumber=#{sn}&country=USA&fullCountryName=United%20States"
-
-  #  Next fetch warranty information from Apple, convert the resulting json to a hash
-  warranty_info = eval %x{curl -k -s "#{url}"}.split(',').each { |element|
-    element.sub! ':', ' => '
-  }.join(',') 
+# This is a complete hack to disregard SSL Cert validity for the Apple
+#  Selfserve site.  We had SSL errors as we're behind a proxy.  I'm
+#  open suggestions for doing it 'Less-Hacky.'
+module OpenSSL
+  module SSL
+    remove_const:VERIFY_PEER
+  end
 end
 
-#  Set the Purchase Date by searching through the Array
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
+
+# Constraining to the Darwain Kernel - it's only useful for Macs
+if Facter.value('kernel') == 'Darwin'
+  warranty_array = []
+  sn  = %x{ioreg  -l | grep IOPlatformSerialNumber | sed -e s/\\"//g}.chomp.split{"="}[-1].chomp
+  open('https://selfsolve.apple.com/warrantyChecker.do?sn=' + sn.upcase + '&country=USA') {|item|
+         item.each_line {|item|}
+         warranty_array = item.strip.split('"')
+  }
+  coverage = warranty_array.index('COV_END_DATE')
+  purchase = warranty_array.index('PURCHASE_DATE')
+  outofwarranty = warranty_array.index('HW_COVERAGE_DESC')
+end
+
 Facter.add("purchase_date") do
   confine :kernel => "Darwin"
   setcode do
-    warranty_info['PURCHASE_DATE'].gsub("-", ".")
+    warranty_array[purchase+2]
   end 
 end
 
-#  First we need to check and see if the machine is out of warranty.  If it is,
-#  return the purchase date.  If it isn't out of warranty, return the Coverage Date
 Facter.add("warranty_out") do
   confine :kernel => "Darwin"
   setcode do
-    warranty_info['COVERAGE_DESC'] == 'Out of Warranty' ? 'yes' : 'no'
+    (warranty_array[outofwarranty+2] == 'Out of Warranty') ? "Yes" : "No"
   end 
 end
 
@@ -32,6 +44,6 @@ end
 Facter.add("warranty_end") do
   confine :kernel => "Darwin"
   setcode do
-    warranty_info['COVERAGE_DATE'].gsub("-", ".")
+    (!warranty_array[coverage+2].empty?) ? warranty_array[coverage+2] : "Expired"
   end 
 end
