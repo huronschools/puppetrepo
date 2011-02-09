0e361ce6a8831efd0ff8404f0059ea56b3351ddc
Gary Larizza <gary@huronhs.com>
2011-02-09 07:49:15 -0500

    Added documentation and refactored to hash all the important selfserve values

--- modules/custom/lib/facter/warranty.rb
+++ modules/custom/lib/facter/warranty.rb
@@ -1,5 +1,14 @@
+#
+# File:       warranty.rb
+# Type:       Facter fact
+# Decription: Returns the dates of purchase and warranty expiration,
+#              plus determines if the warranty is active or inactive.
+#              We're using open-uri which caused SSL errors behind 
+#              proxies (hence the hack in lines 16-20).
+
 require 'facter'
 require 'open-uri'
+require 'openssl'
 
 # This is a complete hack to disregard SSL Cert validity for the Apple
 #  Selfserve site.  We had SSL errors as we're behind a proxy.  I'm
@@ -16,34 +25,34 @@ OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 # Constraining to the Darwin Kernel - it's only useful for Macs
 if Facter.value('kernel') == 'Darwin'
   warranty_array = []
+  hash = {}
   sn  = %x{system_profiler SPHardwareDataType | awk -F": " '/Serial/{print $2}'}
   open('https://selfsolve.apple.com/warrantyChecker.do?sn=' + sn.upcase + '&country=USA') {|item|
          item.each_line {|item|}
          warranty_array = item.strip.split('"')
+         warranty_array.each {|array_item|
+           hash[array_item] = warranty_array[warranty_array.index(array_item) + 2] if array_item =~ /[A-Z][A-Z\d]+/
+         }
   }
-  coverage = warranty_array.index('COV_END_DATE')
-  purchase = warranty_array.index('PURCHASE_DATE')
-  outofwarranty = warranty_array.index('HW_COVERAGE_DESC')
 end
 
 Facter.add("purchase_date") do
   confine :kernel => "Darwin"
   setcode do
-    warranty_array[purchase+2].gsub("-",".")
+    hash['PURCHASE_DATE'].gsub("-",".")
   end 
 end
 
 Facter.add("warranty_out") do
   confine :kernel => "Darwin"
   setcode do
-    (warranty_array[outofwarranty+2] == 'Out of Warranty') ? "Yes" : "No"
+    (hash['HW_COVERAGE_DESC'] == 'Out of Warranty') ? "Yes" : "No"
   end 
 end
-
 
 Facter.add("warranty_end") do
   confine :kernel => "Darwin"
   setcode do
-    (!warranty_array[coverage+2].empty?) ? warranty_array[coverage+2].gsub("-",".") : "Expired"
+    (!hash['COV_END_DATE'].empty?) ? hash['COV_END_DATE'].gsub("-",".") : "Expired"
   end 
 end
