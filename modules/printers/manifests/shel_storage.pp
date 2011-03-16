f6acb5a22dfdec358d10d4bc9ac547af379f9b8a
Gary Larizza <gary@huronhs.com>
2011-03-16 07:17:10 -0400

    Added final variable, single quotes to exec, and fixed indenting

--- modules/printers/manifests/shel_storage.pp
+++ modules/printers/manifests/shel_storage.pp
@@ -4,28 +4,29 @@ class printers::shel_storage {
 	require printers::params
 	require printers::drivers
 	
-	$printer_name = $operatingsystem ? {
+	$printer_name 			= $operatingsystem ? {
 		darwin		=> "psm_SHEL_Storage",
 		centos		=> "Shawnee_Storage_Copier",
 	}
-	$printer_location = "Shawnee\\ Staff\\ Storage"
-	$printer_destination = "Shawnee\\ Storage\\ Copier"
-	$lpd_ip = "lpd://10.13.0.3/Shawnee_Storage_Copier"
-	$options = "printer-is-shared=false"
-	$printer_command = "/usr/sbin/lpadmin -p $printer_name -L $printer_location -D $printer_destination -v $lpd_ip -P '${printers::drivers::hp_lj9040_path}' -E -o $options"
-	$printer_check = "/usr/bin/lpstat -a $printer_name"
+	$printer_ppd			= "psm_SHEL_Storage.ppd"
+	$printer_location 		= "Shawnee Staff Storage"
+	$printer_destination 	= "Shawnee Storage Copier"
+	$lpd_ip 				= "lpd://10.13.0.3/Shawnee_Storage_Copier"
+	$options 				= "printer-is-shared=false"
+	$printer_command 		= "/usr/sbin/lpadmin -p $printer_name -L '$printer_location' -D '$printer_destination' -v $lpd_ip -P '${printers::drivers::hp_lj9040_path}' -E -o $options"
+	$printer_check 			= "/usr/bin/lpstat -a $printer_name"
 	
 	exec { "Shawnee_Storage_Copier":
 		command 	=> "$printer_command",
-		before 		=> File["/etc/cups/ppd/psm_SHEL_Storage.ppd"],
+		before 		=> File["/etc/cups/ppd/$printer_ppd"],
 		unless 		=> "$printer_check",
 	}
 
-	file { "/etc/cups/ppd/psm_SHEL_Storage.ppd":
+	file { "/etc/cups/ppd/$printer_ppd":
 		owner 		=> "${printers::params::print_owner}",
 		group 		=> "${printers::params::print_group}",
 		mode 		=> 644,
-		source	 	=> "puppet:///printers/PPDs/psm_SHEL_Storage.ppd",
+		source	 	=> "puppet:///printers/PPDs/$printer_ppd",
 		ensure 		=> present,
 		require		=> Exec["Shawnee_Storage_Copier"],
 	}
--- modules/printers/manifests/shel_workroom.pp
+++ modules/printers/manifests/shel_workroom.pp
@@ -4,28 +4,29 @@ class printers::shel_workroom{
 	require printers::params
 	require printers::drivers
 	
-	$printer_name = $operatingsystem ? {
+	$printer_name 			= $operatingsystem ? {
 		darwin		=> "psm_SHEL_Workroom",
 		centos		=> "Shawnee_Workroom_Copier",
 	}
-	$printer_location = "Shawnee\\ Staff\\ Workroom"
-	$printer_destination = "Shawnee\\ Workroom\\ Copier"
-	$lpd_ip = "lpd://10.13.0.3/Shawnee_Workroom_Copier"
-	$options = "printer-is-shared=false"
-	$printer_command = "/usr/sbin/lpadmin -p $printer_name -L $printer_location -D $printer_destination -v $lpd_ip -P '${printers::drivers::hp_lj9040_path}' -E -o $options"
-	$printer_check = "/usr/bin/lpstat -a $printer_name"
+	$printer_ppd			= "psm_SHEL_Workroom.ppd"
+	$printer_location 		= "Shawnee Staff Workroom"
+	$printer_destination 	= "Shawnee Workroom Copier"
+	$lpd_ip 				= "lpd://10.13.0.3/Shawnee_Workroom_Copier"
+	$options 				= "printer-is-shared=false"
+	$printer_command 		= "/usr/sbin/lpadmin -p $printer_name -L '$printer_location' -D '$printer_destination' -v $lpd_ip -P '${printers::drivers::hp_lj9040_path}' -E -o $options"
+	$printer_check 			= "/usr/bin/lpstat -a $printer_name"
 	
 	exec { "Shawnee_Workroom_Copier":
 		command 	=> "$printer_command",
-		before 		=> File["/etc/cups/ppd/psm_SHEL_Workroom.ppd"],
+		before 		=> File["/etc/cups/ppd/$printer_ppd"],
 		unless 		=> "$printer_check",
 	}
 
-	file { "/etc/cups/ppd/psm_SHEL_Workroom.ppd":
+	file { "/etc/cups/ppd/$printer_ppd":
 		owner 		=> "${printers::params::print_owner}",
 		group 		=> "${printers::params::print_group}",
 		mode 		=> 644,
-		source 		=> "puppet:///printers/PPDs/psm_SHEL_Workroom.ppd",
+		source 		=> "puppet:///printers/PPDs/$printer_ppd",
 		ensure 		=> present,
 		require 	=> Exec["Shawnee_Workroom_Copier"],
 	}
