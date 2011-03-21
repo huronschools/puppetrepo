0267800851ae1c2848b37f82ad9b7ac0e993a0f0
Gary Larizza <gary@huronhs.com>
2011-03-21 09:03:38 -0400

    Modified the External Node Classifier to use our /etc/facts.txt file

--- files/rubyexternalnode.rb
+++ files/rubyexternalnode.rb
@@ -2,58 +2,41 @@
 #
 #
 #  This External Node classifier script will return appropriate classes for Puppet depending
-#   on the machine's hostname (i.e. testing-shawstaff-shel.huronhs.com will return "shawstaff"
-#   and "shel" in YAML). If the machine's hostname doesn't match this format, it returns a blank
-#   YAML block so that the nodes.pp file can be consulted
+#   on the machine's huron_class fact.  The script will dip into the Server's YAML depository,
+#   search for the passed certname, extract the fact information, and return a YAML block to
+#   puppet.
 #
 #  Created by Gary Larizza - 6.10.2010
-#  Last Modified - 6.10.2010
+#  Last Modified - 3.21.2011
 #
 #
 
 require 'yaml'
 require 'puppet'
 
-
-# Initialize variables
-function = nil
+# Intitialize Variables
+vardir = Puppet[:vardir]
+yamldir = "#{vardir}/yaml/facts/"
 default = {'classes' => []}
+yaml_output = {}
 
-# Check to see if either supported YAML Facts store exists.
-#  Updated for 2.6.2 $vardir
+# Check to see if the Node YAML file exists.
+# If it exists, set the huron_class variable to that fact's value
 begin
-  if File.exists?("/var/lib/puppet/yaml/facts") && File.directory?("/var/lib/puppet/yaml/facts")
-    vardir_path = "/var/lib/puppet/yaml/facts/"
-  elsif File.exists?("/var/puppet/yaml/facts") && File.directory?("/var/puppet/yaml/facts")
-    vardir_path = "/var/puppet/yaml/facts/"
-  else
-    exit(1)
-  end
-rescue Exception => factdir
-  puts "YAML Facts directory not found!"
-  exit(1)
-end
-
-# Check to see if the Node YAML file exists, based on certname
-begin
-  yamlfile = YAML::load_file(vardir_path + ARGV[0] + '.yaml').values
-  fqdn = yamlfile["fqdn"]
+  yamlfile = YAML::load_file(yamldir + ARGV[0] + '.yaml').values
+  huron_class = yamlfile["huron_class"]
+  environment = yamlfile["environment"]
 rescue Exception => error
   puts "Node YAML file was not found!  Error: " + error
   exit(1)
 end
 
-# Split out the appropriate classes from the machine's hostname
-function = %x{echo #{fqdn} | awk -F '-' '{print $2}'}.chomp
-building = %x{echo #{fqdn} | awk -F '-' '{print $3}'}.chomp.gsub!(/[.].*/,"")
-
-# If the hostname doesn't contain classes, pass a blank YAML block so the
-#  nodes.pp file can check for appropriate classes.
-if function + "xxx" == "xxx"
+# If huron_class is nil or "nodes.pp", escape with a blank YAML output
+if huron_class.nil? or huron_class == "nodes.pp"
   print default.to_yaml
   exit(0)
 end
 
-# Finally, pass a YAML block containing the appropriate classes
-output = {'classes' => [ 'general_image', building , function ]}
-print output.to_yaml
+# Output our classes and environment values to YAML for Puppet
+yaml_output =  {'classes' => huron_class.split(','), 'environment' => environment}
+print yaml_output.to_yaml
