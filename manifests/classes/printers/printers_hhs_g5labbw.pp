a90bf7b243c5a2b6c11438dc15e7185c35fdfbdd
Huron Schools <gary@huronhs.com>
2011-05-06 13:36:24 -0400

    Committed a change and updated hash-rocket alignment

--- manifests/classes/printers/printers_hhs_g5labbw.pp
+++ manifests/classes/printers/printers_hhs_g5labbw.pp
@@ -4,17 +4,17 @@ class printers_hhs_g5labbw {
 
 	exec { "hhs_g5lab_bw":
 		command => "/usr/sbin/lpadmin -p psm_HHS_G5_BW -L HHS\\ Room\\ 099 -D HHS\\ G5\\ Lab\\ Black\\ and\\ White\\ Printer -v lpd://10.13.1.8/HHS_G5_Lab_BW_Printer -P /Library/Printers/PPDs/Contents/Resources/HP\\ LaserJet\\ 4200\\ Series.gz -E -o printer-is-shared=false",
-		unless => "/usr/bin/lpstat -a psm_HHS_G5_BW",
+		unless 	=> "/usr/bin/lpstat -a psm_HHS_G5_BW",
 		require => File["/Library/Printers/PPDs/Contents/Resources/HP LaserJet 4200 Series.gz"],
 	}
 
 	file { "/Library/Printers/PPDs/Contents/Resources/HP LaserJet 4200 Series.gz":
-		owner => "root",
-		group => "admin",
-		mode => 664,
-		source => "puppet:///files/HP LaserJet 4200 Series.gz",
-		ensure => present,
-		before => Exec["hhs_g5lab_bw"],
+		owner 	=> "root",
+		group 	=> "admin",
+		mode 	=> 664,
+		source 	=> "puppet:///files/HP LaserJet 4200 Series.gz",
+		ensure 	=> present,
+		before 	=> Exec["hhs_g5lab_bw"],
 		replace => false,
 	}
 
