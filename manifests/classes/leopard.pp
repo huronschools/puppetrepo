d3ec5d6f6ba6aa1c2168e1b28e1618afc118d4fd
System Administrator <root@testing.huronhs.com>
2011-12-30 16:23:02 -0500

    Add App Usage Fact
    Add the app usage fact and clean up leopard.pp and snowleopard.pp

--- manifests/classes/leopard.pp
+++ manifests/classes/leopard.pp
@@ -5,24 +5,24 @@
 
 class leopard {
 	# Package Names
-	$ilife = "iLife09.dmg"
-	$iwork = "iWork09.dmg"
-	$serveradmin = "ServerAdmin1057.dmg"
-	$timezone = "Timezone.dmg"	
-	$office = "Office2004.dmg"
+	$ilife         = "iLife09.dmg"
+	$iwork 	       = "iWork09.dmg"
+	$serveradmin   = "ServerAdmin1057.dmg"
+	$timezone      = "Timezone.dmg"	
+	$office        = "Office2004.dmg"
 	$officeupdates = "OfficeUpdates.dmg"
 
 	# Package Calls
-	package{"$serveradmin": source => "$pkg_base/$serveradmin",}
-    package{"$ilife": 		source => "$pkg_base/$ilife",}
-    package{"$iwork": 		source => "$pkg_base/$iwork",}
-	package{"$timezone": 	source => "$pkg_base/$timezone",}
+	package{"$serveradmin": source => "$::pkg_base/$serveradmin",}
+        package{"$ilife": 	source => "$::pkg_base/$ilife",}
+        package{"$iwork": 	source => "$::pkg_base/$iwork",}
+	package{"$timezone": 	source => "$::pkg_base/$timezone",}
 	package{"$office": 
-		source 	=> "$pkg_base/$office",
+		source 	=> "$::pkg_base/$office",
 		before 	=> Package[$officeupdates],
 	}
 	package{"$officeupdates": 
-		source 	=> "$pkg_base/$officeupdates",
+		source 	=> "$::pkg_base/$officeupdates",
 		require => Package[$office],
 	}
 
--- manifests/classes/snowleopard.pp
+++ manifests/classes/snowleopard.pp
@@ -8,28 +8,28 @@ class snowleopard {
 	include office2008
 
 	# Package Names
-	$ilife = "iLife09.dmg"
-	$iwork = "iWork09.dmg"
+	$ilife       = "iLife09.dmg"
+	$iwork       = "iWork09.dmg"
 	$serveradmin = "ServerAdminTools10.6.dmg"
-	$timezone = "Timezone.dmg"	
-	$rosetta = "Rosetta.dmg"
-	$flash = "Flash_Installer_64_Bit-20100920.dmg"
-	$shockwave = "Shockwave64bit.dmg"
-	$gbloops = "GaragebandLoops.dmg"
+	$timezone    = "Timezone.dmg"	
+	$rosetta     = "Rosetta.dmg"
+	$flash       = "Flash_Installer_64_Bit-20100920.dmg"
+	$shockwave   = "Shockwave64bit.dmg"
+	$gbloops     = "GaragebandLoops.dmg"
 
 	# Set Package resource defaults for OS X clients
 	Package{ensure => installed,provider => pkgdmg}
 
 	# Package Calls
-	package{"$serveradmin": source => "$pkg_base/$serveradmin",}
-    package{"$ilife": source => "$pkg_base/$ilife",}
+	package{"$serveradmin": source => "$::pkg_base/$serveradmin",}
+    	package{"$ilife": source => "$::pkg_base/$ilife",}
 	package{"$gbloops":
-			source 	=> "$pkg_base/$gbloops",
+			source 	=> "$::pkg_base/$gbloops",
 			require	=> Package["$ilife"],
 		}
-    package{"$iwork": source => "$pkg_base/$iwork",}
-	package{"$timezone": source => "$pkg_base/$timezone",}
-	package{"$rosetta": source =>"$pkg_base/$rosetta",}
-	package{"$flash": source =>"$pkg_base/$flash",}
-	package{"$shockwave": source =>"$pkg_base/$shockwave",}
+    	package{"$iwork": source => "$::pkg_base/$iwork",}
+	package{"$timezone": source => "$::pkg_base/$timezone",}
+	package{"$rosetta": source =>"$::pkg_base/$rosetta",}
+	package{"$flash": source =>"$::pkg_base/$flash",}
+	package{"$shockwave": source =>"$::pkg_base/$shockwave",}
 }
